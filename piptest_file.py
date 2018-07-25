334b473e56441d8a6e9b1171581a51da49406e62
nikunj08 <nikunjaggarwal08@gmail.com>
2018-07-25 12:45:06 +0530

    Add files via upload
    first commit

---
+++ piptest_file.py
@@ -0,0 +1,4 @@
+import requests
+
+def hello_world():
+    print("hello world")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pipinstalltesting',
+      version='0.1',
+      description='pipinstalltesting',
+      url='http://github.com/storborg/funniest',
+      author='Nikunj',
+      author_email='nikunjaggarwal08@gmail.com',
+      license='Edge',
+      packages=['pipinstall_testing'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
